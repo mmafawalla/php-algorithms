a0bf70adc9dfdb61103e6b1ccd1f9afe1ab9293f
Muhammed Mafawalla <31379283+mmafawalla@users.noreply.github.com>
2018-06-01 21:17:35 +1000

    First Commit

---
+++ rank_popular_words_string/index.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+Initialisation
+*/
+
+$str = "";
+$arr = array();
+$temporary_arr = array();
+
+$str = "Lorem ipsum dolor dolor amet, suscipit dolor elit. suscipit eu scelerisque,
+orci. Vivamus suscipit nisi suscipit";
+
+//create an array from str separated by space
+$str = explode(" ", $str);
+
+echo "\n \n";
+
+//iterate through str array
+for ($i = 0; $i < count($str) ; $i++) {
+  //Check if current word is in second array
+  if(array_key_exists($str[$i], $arr)){
+    //increment its value
+    $arr[$str[$i]] += 1;
+  }else{
+    //add to second array
+    $arr += [$str[$i] => 1];
+  }
+
+}
+
+//sort second array according to value
+arsort($arr);
+
+echo "\n \n";
+//dump ranked, sorted array
+print_r($arr);
+echo "\n \n";
+
+
+/*
+/Users/muhammedmafawalla/Documents/Coding/php/playground/list_popular_words_in_string.php
+*/
+
+
+
+
+
+
+?>
