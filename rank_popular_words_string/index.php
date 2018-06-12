0e7eb00ff6385ca50df61d550a2b526b45843e43
“Muhammed <mafawall27@gmail.com>
2018-06-12 21:27:03 +1000

    General file cleaning up

--- rank_popular_words_string/index.php
+++ rank_popular_words_string/index.php
@@ -3,10 +3,12 @@
 File Name: index.php
 Project Name: Rank popular words in string
 Author: Muhammed Mafawalla
-Version: 1.1
+Version: 1.1.5
 Author URL: https://mmafawalla.com/
-Description: Business WordPress Theme
-License: GNU General Public License
+Author Github URL: https://github.com/mmafawalla
+Summary: Splits str to array, iterates while comparing against
+a second array, finally sorts according to value descending
+License: GNU General Public License v3.0
 
 */
 
@@ -16,8 +18,12 @@ $str = "";
 $arr = array();
 $temporary_arr = array();
 
-$str = "Lorem ipsum dolor dolor amet, suscipit dolor elit. suscipit eu scelerisque,
-orci. Vivamus suscipit nisi suscipit";
+$str = "Apple stock traded lower by more than 1 percent Friday morning, but Bank of
+America analyst Wamsi Mohan says investors shouldn't jump to conclusions based on the report.
+Mohan said initial media reports in 2017 suggested iPhone orders of well over 100 million units.
+Those estimates were then revised to about 100 million before the launch and then revised again
+between 60 million and 80 million after the iPhone X launch was delayed.
+";
 
 //create an array from str separated by space
 $str = explode(" ", $str);
@@ -44,10 +50,5 @@ echo "\n \n";
 //dump ranked, sorted array
 print_r($arr);
 echo "\n \n";
-
-
-
-
-
 
 ?>
